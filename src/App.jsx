a0e7e47187da61b2d5d31885ccc474573dc7e818
Mahesh22500 <maheshsingh22500@gmail.com>
2024-12-11 18:38:46 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,63 +12,63 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import Protected from "./components/Protected";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Protected>
+          <Navbar></Navbar>
+          <Home></Home>
+        </Protected>
+      </>
+    ),
+  },
+  {
+    path: "/signup",
+    element: <Signup></Signup>,
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/blog/:id",
+
+    element: (
+      <>
+        <Protected>
+          <Navbar></Navbar>
+          <Blog></Blog>,
+        </Protected>
+      </>
+    ),
+  },
+  {
+    path: "/userBlogs",
+    element: (
+      <>
+        <Protected>
+          <Navbar></Navbar>
+
+          <UserBlogs></UserBlogs>
+        </Protected>
+      </>
+    ),
+  },
+  {
+    path: "/addform",
+    element: (
+      <>
+        <Protected>
+          <Navbar></Navbar>
+          <AddBlogForm></AddBlogForm>,
+        </Protected>
+      </>
+    ),
+  },
+]);
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-          <Protected>
-            <Navbar></Navbar>
-            <Home></Home>
-          </Protected>
-        </>
-      ),
-    },
-    {
-      path: "/signup",
-      element: <Signup></Signup>,
-    },
-    {
-      path: "/login",
-      element: <Login></Login>,
-    },
-    {
-      path: "/blog/:id",
-
-      element: (
-        <>
-          <Protected>
-            <Navbar></Navbar>
-            <Blog></Blog>,
-          </Protected>
-        </>
-      ),
-    },
-    {
-      path: "/userBlogs",
-      element: (
-        <>
-          <Protected>
-            <Navbar></Navbar>
-
-            <UserBlogs></UserBlogs>
-          </Protected>
-        </>
-      ),
-    },
-    {
-      path: "/addform",
-      element: (
-        <>
-          <Protected>
-            <Navbar></Navbar>
-            <AddBlogForm></AddBlogForm>,
-          </Protected>
-        </>
-      ),
-    },
-  ]);
 
   return (
     <>
